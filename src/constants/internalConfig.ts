e44be97ff7f6b7dc20108eedd1111ad353c7cf3c
Olivier Kastel <olivier.kastel@gmail.com>
2017-06-01 13:54:44 -0700

    changed auth-server-token name

--- src/constants/internalConfig.ts
+++ src/constants/internalConfig.ts
@@ -1,2 +1,2 @@
-export const COOKIE_AUTH_TOKEN_KEY: string = 'auth_token';
+export const COOKIE_AUTH_TOKEN_KEY: string = 'auth-server-token';
 export const INITIAL_STATE_KEY: string = '__INITIAL_STATE__';
